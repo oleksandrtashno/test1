3741b08bc0fd4d59778c2a1fba2f6ce237376579
alex <oleksandr.tashno@gmail.com>
2021-01-05 00:08:05 +0200

    wnafnefnrer

--- main/main.c
+++ main/main.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "esp_sntp.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_wifi.h"
-#include "esp_event.h"
-#include "esp_event_loop.h"
-#include "esp_wifi.h"
 #include "nvs_flash.h"
 #include "esp_log.h"
-#include "esp_http_client.h"
 #include "protocol_examples_common.h"
-#include "freertos/task.h"
 
-#define MAX_APs 20
+#define TAG	"NTP_TIME"
+
+void on_time_received(struct timeval *tv)
+{
+	setenv("TZ", "EET-2EEST,M3.5.0/3,M10.5.0/4", 1);
+	tzset();
+	struct tm *timeinfo = localtime(&tv->tv_sec);
+
+	char buffer[64];
+
+	strftime(buffer, sizeof(buffer), "%c", timeinfo);
+	ESP_LOGI(TAG, "Time now is %s", buffer);
+
+	example_disconnect();
+}
 
 void app_main(void)
 {
-	//uint32_t stack_size = uxTaskGetStackHighWaterMark(NULL);
-	uint32_t stack_size = 32768;
+	nvs_flash_init();
+	tcpip_adapter_init();
+	esp_event_loop_create_default();
+	example_connect();
 
-	printf("Allocating %d bytes for main task\n", stack_size);
-
-	//xTaskCreate(&heartbeat, "heartbeat", 2048, NULL, 1, NULL);
-	xTaskCreate(&scan_wifi_aps, "scan wifi aps", stack_size, NULL, 2, NULL);
+	sntp_set_sync_mode(SNTP_SYNC_MODE_IMMED);
+	sntp_setservername(0, "pool.ntp.org");
+	sntp_init();
+	sntp_set_time_sync_notification_cb(on_time_received);
 }

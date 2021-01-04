9a6cd4ff82ebef5530a780a22b26e7ec534433b0
alex <oleksandr.tashno@gmail.com>
2021-01-04 23:33:38 +0200

    now it works

--- main/main.c
+++ main/main.c
@@ -11,7 +11,13 @@
 #include "protocol_examples_common.h"
 #include "freertos/task.h"
 
-#define AX_APs 20
+#define MAX_APs 20
+
+static char *getAuthModeName(wifi_auth_mode_t auth_mode)
+{
+  char *names[] = {"OPEN", "WEP", "WPA PSK", "WPA2 PSK", "WPA WPA2 PSK", "MAX"};
+  return names[auth_mode];
+}
 
 static esp_err_t event_handler(void *ctx, system_event_t *event)
 {
@@ -23,11 +29,41 @@ void wifi_init()
 	ESP_ERROR_CHECK(nvs_flash_init());
 	tcpip_adapter_init();
 	ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+
+	wifi_init_config_t wifi_config = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK(esp_wifi_init(&wifi_config));
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+	ESP_ERROR_CHECK(esp_wifi_start());
 }
 
 void scan_wifi_aps(void *params)
 {
 	wifi_init();
+
+	wifi_scan_config_t scan_config = {
+		.ssid = 0,
+		.bssid = 0,
+		.channel = 0,
+		.show_hidden = true,
+		.scan_time = {.active = {.min = 1000, .max = 1300}},
+		.scan_type = WIFI_SCAN_TYPE_ACTIVE
+	};
+
+	ESP_ERROR_CHECK(esp_wifi_scan_start(&scan_config, true));
+
+	wifi_ap_record_t scan_results[MAX_APs];
+
+	uint16_t ap_count = MAX_APs;
+
+	ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&ap_count, scan_results));
+
+	printf("Found %d access points:\n", ap_count);
+	printf("\n");
+	printf("               SSID              | Channel | RSSI |   Auth Mode \n");
+	printf("----------------------------------------------------------------\n");
+	for (int i = 0; i < ap_count; i++)
+		printf("%32s | %7d | %4d | %12s\n", (char *)scan_results[i].ssid, scan_results[i].primary, scan_results[i].rssi, getAuthModeName(scan_results[i].authmode));
+	printf("----------------------------------------------------------------\n");
 
 	while (true)
 	{
@@ -43,5 +79,5 @@ void app_main(void)
 	printf("Allocating %d bytes for main task\n", stack_size);
 
 	//xTaskCreate(&heartbeat, "heartbeat", 2048, NULL, 1, NULL);
-	//xTaskCreate(&scan_wifi_aps, "scan wifi aps", stack_size, NULL, 2, NULL);
+	xTaskCreate(&scan_wifi_aps, "scan wifi aps", stack_size, NULL, 2, NULL);
 }
